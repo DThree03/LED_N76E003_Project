c3544c831ef4873a2447dedb7c2ac26b520a42cf
DatVo <dat.vo@wecheer.io>
2022-12-24 13:42:34 +0700

    Update release code

--- ProjectBuild/Code/main.c
+++ ProjectBuild/Code/main.c
@@ -12,7 +12,7 @@ static unsigned char    read_type;
 static unsigned char    read_addr;
 unsigned char*          read_buff = NULL;
 
-static unsigned char    DATA_LED_7[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5};
+static unsigned char    DATA_LED_7[16] = {21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21};
 static unsigned char    DATA_LED_XL[4] = {0, 0, 0, 0};
 /*****************************************************************************
 * Main Loop
